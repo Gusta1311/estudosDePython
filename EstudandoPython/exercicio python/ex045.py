e8d066dc90fed7b236db21e2ee2e0efe1d9d689a
Gustavo Cruz <gc4206068@gmail.com>
2025-02-20 10:51:05 -0300

    Atualização 4.0

--- EstudandoPython/exercicio python/ex045.py
+++ EstudandoPython/exercicio python/ex045.py
@@ -1,1 +1,30 @@
 #Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento: À vista dinheiro/cheque: 10% de desconto, À vista no cartão: 5% de desconto, Em até 2x no cartão: preço normal, 3x ou mais no cartão: 20% de juros
+print('Abrindo Programa de Calculo de Preço de Produto')
+
+#Perguntar o valor do produto
+valor = float(input('Valor do produto: '))
+
+#Escolher forma de pagamento
+pagamento = str(input('Escolha sua forma de pagamento: 1- Dinheiro, 2- Cartão, 3- 2x, 4-3x: '))
+
+#Fazer valor nomal a vista dinheiro/cheque com 10% de desconto
+dinheiro = valor - (valor*10/100)
+
+#Fazer valor normal a vista no cartão com 5% de desconto
+cartao_vista = valor - (valor*5/100)
+cartao2 = valor 
+cartao3 = valor - (valor*20/100)
+
+#fazer as condiçoes de pagamento
+if pagamento == 'Dinheiro':
+    print(f'O valor em dinheiro foi de {dinheiro} R$ com 10% desconto')
+elif pagamento == 'Cartão':
+    print (f'O valor no cartão a vista foi de {cartao_vista} R$ com 5% de desconto ')
+elif pagamento == '2x':
+    print(f'O valor do produto em 2x no cartão foi de {cartao2} R$')
+elif pagamento == '3x':
+    print(f'O valor no cartão em 3x ou mais no cartão foi de {cartao3} R$ com 20% de juros.')
+else:
+    print('Opção Inválida.')
+print('Fim do programa')
+
--- EstudandoPython/exercicio python/ex046.py
+++ EstudandoPython/exercicio python/ex046.py
@@ -1,1 +1,21 @@
 #Crie um programa que faça o computador jogar Jokenpô com você
+from random import choice
+print('Vamos jogar Jokenpô')
+amauri = str(input('Escolha entre pedra papel e tesoura: '))
+
+computador = ['pedra', 'papel', 'tesoura']
+escolha = choice(computador)
+
+if amauri == escolha:
+    print('Empate')
+elif amauri == 'pedra' and escolha == 'tesoura':
+    print('Você ganhou!')
+elif amauri == 'tesoura' and escolha == 'papel':
+    print('Você ganhou!')
+elif amauri == 'papel' and escolha == 'pedra':
+    print('Você ganhou!')
+else:
+    print('Você perdeu!')
+    print(f'Você escolheu {amauri} e o computador escolheu {escolha}!')
+print('Fim de jogo!')
+
